b60c658e02b87d9ab364c8a2b0e261292470e356
OmkarSawant25 <omkarbro2525@gmail.com>
2026-02-19 12:09:38 +0530

    Recursion Level 4

---
+++ Recursion/maxSumOfNonAdjacentElements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+void solve(vector<int> &arr, int i, int sum, int &maxi)
+{
+    // Base case
+    if (i >= arr.size())
+    {
+        // Update maxi
+        maxi = max(maxi, sum);
+        return;
+    }
+    // Include
+    solve(arr, i + 2, sum + arr[i], maxi);
+    // Exclude
+    solve(arr, i + 1, sum, maxi);
+}
+
+int main()
+{
+    vector<int> arr{2, 1, 4, 9};
+    int sum = 0;
+    int maxi = INT_MIN;
+    int i = 0;
+    solve(arr, i, sum, maxi);
+
+    cout << "Answer is : " << maxi << endl;
+}
